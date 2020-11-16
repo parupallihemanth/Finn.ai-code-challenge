43931437ba76ccc0d9fb7a55b5bc94c7beabcd83
Hemanth Babu Parupalli <hemanth@nuenergy.ai>
2020-11-16 06:10:25 -0400

    final commit

--- Express.Server/server.js
+++ Express.Server/server.js
@@ -15,21 +15,6 @@ app.use(cors());
 
 app.use("/api", userRoutes);
 
-// deployment setup
-const __dirrname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirrname, "frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirrname, "./frontend/build/index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
-
 // Asigning port to express server
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node Express.Server/server.js
